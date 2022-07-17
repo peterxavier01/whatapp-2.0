b45441fe320fc9728ad8ec247d810e16c0e0b96d
Peter Uadiale <peteuadiale1@gmail.com>
2022-07-17 21:26:07 +0100

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFsmJLFC2lzayT3X0CRnXggqCsSYPt52s",
-  authDomain: "whatsapp-2-8116c.firebaseapp.com",
-  projectId: "whatsapp-2-8116c",
-  storageBucket: "whatsapp-2-8116c.appspot.com",
-  messagingSenderId: "881605363960",
-  appId: "1:881605363960:web:2fae327ab6ee1bc8c6d30d",
-  measurementId: "G-YHVCYHMPZB",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
